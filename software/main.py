93acf2519315bcbddc26dd1305ce18f915fae614
Tudor M. <marcutudor5@gmail.com>
2025-01-08 21:21:47 +0200

    Refactor main.py code to use cosine similarity

--- software/main.py
+++ software/main.py
@@ -13,7 +13,7 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval().to('cpu')
     Function to compute the face weights
     based on the img_path
 '''
-def compute_face_weight(img_path):
+def compute_face_embedding(img_path):
     img = Image.open(img_path)
     boxes, _ = mtcnn.detect(img)
     if boxes is not None:
@@ -36,26 +36,60 @@ def get_image_paths(database_path):
             image_paths.append(os.path.join(root, name))
             
     return image_paths
+
+'''
+    Function used to compute the cosine similarity between
+    to embeddings
     
-def calculate_distance(embedding1, embedding2):
-    return np.linalg.norm(embedding1 - embedding2)
+    returns -1 if they are opposite
+    returns 1 if they are identical
+'''
+def compute_similarity(embedding1, embedding2):
+    dot_product = np.dot(embedding1[0], embedding2[0])
+    
+    # compute the magnitude of each embedding
+    norm_embedding1 = np.linalg.norm(embedding1[0])
+    norm_embedding2 = np.linalg.norm(embedding2[0])
+    
+    # compute the cosine similarity
+    similarity = dot_product / (norm_embedding1 * norm_embedding2)
+    
+    return similarity
 
 if __name__ == '__main__':
-    image_paths_testing = get_image_paths("database_testing_photos/")
+    image_paths_testing   = get_image_paths("database_testing_photos/")
     image_paths_reference = get_image_paths("database_reference_photos/")
+    reference_embeddings  = []
+    test_embeddings       = []
     
-    reference_embeddings = [(compute_face_weight(img_path), img_path) for img_path in image_paths_reference]
-    
-    for test_img in image_paths_testing:
-        test_embedding = compute_face_weight(test_img)
+    # compute the reference embeddings
+    for img in image_paths_reference:
+        embedding = compute_face_embedding(img)
         
-        if test_embedding is not None:
-            distances = [(calculate_distance(test_embedding, ref_embedding), ref_img_path) for ref_embedding, ref_img_path in reference_embeddings]
-            distances.sort(key = lambda x: x[0])
-            closest_images = distances[:3]
-    
-            print(f"\nClosest images to {test_img}:")
-            for dist, img_path in closest_images:
-                print(f"Distance: {dist:.3f}, Reference Image Path: {img_path}")
+        # append the embedding to the list
+        reference_embeddings.append([embedding,img])
+        
+    # go through the test images and compute the embeddings
+    for img in image_paths_testing:
+        embedding = compute_face_embedding(img)
+        
+        # append the embedding to the list
+        test_embeddings.append([embedding, img])
+        
+        
+    # go through the test_embeddings and find the closest refernce_embedding
+    for embedding in test_embeddings:
+        max_similarity = [-1, ""]
+        
+        for ref_embedding in reference_embeddings:
+            similarity = compute_similarity(embedding[0], ref_embedding[0])
+            similarity_max = max_similarity[0]
+            
+            if (similarity > float(max_similarity[0])):
+                max_similarity = [similarity, ref_embedding[1]]
+        
+        
+        print("The image with max similarity to {0} is {1}".format(embedding[1], max_similarity[1]))
+     
     
     
